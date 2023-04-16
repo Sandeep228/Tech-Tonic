52e0f246db7e0ed0ed8c7431ed1226fbae9fb48a
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-04-16 17:46:28 +0530

    fix: solve checkbox problem

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -2,8 +2,15 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppData } from "../constants/Data";
 import { questions } from "../constants/question";
-import { Box, Button, Center, Heading, Checkbox, Select, Input, Text } from '@chakra-ui/react'
-
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  Select,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 
 function CollegeInput() {
   const [option1, setOption1] = useState("");
@@ -12,7 +19,6 @@ function CollegeInput() {
   const [inputValue, setInputValue] = useState("");
   const [isWeb, setIsWeb] = useState(false);
   const [isApp, setIsApp] = useState(false);
-  const [checked,setChecked] = useState(false);
   const [answers, setAnswers] = useState({});
   const [result, setresult] = useState(0);
 
@@ -22,11 +28,8 @@ function CollegeInput() {
     college.toLowerCase().includes(inputValue.toLowerCase())
   );
 
-  const handleCheckboxChange = (e) => {
-    console.log("i am hre",e.target.value);
-    const { id, value, checkedVal } = e.target;
-    setChecked(checkedVal);
-    setAnswers({ ...answers, [id]: checked ? value : undefined });
+  const handleAnswerChange = (questionId, value) => {
+    setAnswers({ ...answers, [questionId]: value });
   };
 
   const formatTargetValue = (data) => {
@@ -69,18 +72,14 @@ function CollegeInput() {
   }
 
   const add = () => {
-    console.log(answers,"answers me hu");
     const sum = Object.values(answers).reduce((total, value) => {
-      console.log("vlue",value);
       return total + Number(value); // add value to total, converting to a number
     }, 0);
 
     setresult(sum);
-    console.log(sum,"after addibg");
     return sum;
   };
   function handlecheck() {
-    console.log("design res",result);
     //check for desgin
     if (result >= 6) {
       return "Advanced";
@@ -120,101 +119,124 @@ function CollegeInput() {
   }
 
   return (
-  <>
-  <Box backgroundColor={"teal"}  w='100%' h='100vh'>
-    <Heading color={"white"} textAlign='center' pt='5'>Welcome to Tech-Tonic</Heading>
-    <Center p='23'>
-    <form onSubmit={handleSubmit} style={{backgroundColor:"white" , borderRadius:"12px", padding:"20px"}} >
-      <label>
-        Time Duration (in months):
-        <Select onChange={handleOption1Change} placeholder='Expected project duration (in months)' required>
-          <option value="short(1-5)">short(1-5)</option>
-          <option value="Medium (5-8)"> Medium (5-8)</option>
-          <option value="long(more than 8 months)">
-            long(more than 8 months)
-          </option>
-        </Select>
-      </label>
-    <br />
-      <label>
-        Team Size:
-        <Select onChange={handleOption2Change} placeholder='Select your team size' required>
-          <option value="Very small (1-5)"> Very small (1-5)</option>
-          <option value="Small (5-10)"> Small (5-10)</option>
-          <option value="Medium (10-50)"> Medium (10-50)</option>
-          <option value="Large (More than 50)"> Large (More than 50)</option>
-        </Select>
-      </label>
-      <br />
-      <div>
-        <label>
-          App Idea:
-          <Input
-            w='xl'
-            type="text"
-            list="colleges"
-            placeholder="Select your project idea"
-            onChange={(e) => setInputValue(e.target.value)}
-            required
-          />
-          <datalist id="colleges">
-            {filteredIdea.map((college) => (
-              <option value={college} key={college} />
-            ))}
-          </datalist>
-        </label>
-      </div>
-   
-      <Text fontSize='2xl'>Designing skills</Text>
-      <div>
-        {questions.map((q) => (
-          <div key={q.id}>
-            <p>{q.question}</p>
-            {q.options.map((option) => (
-              <div key={option.value}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id={q.id}
-                    value={option.value}
-                    //checked={answers[q.id] === option.value}
-                    checked={checked}
-                    onChange={handleCheckboxChange}
-                  />
-                  {option.text}
-                </label>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+    <>
+      <Box backgroundColor={"teal"} w="100%" h="100vh">
+        <Heading color={"white"} textAlign="center" pt="5">
+          Welcome to Tech-Tonic
+        </Heading>
+        <Center p="23">
+          <form
+            onSubmit={handleSubmit}
+            style={{
+              backgroundColor: "white",
+              borderRadius: "12px",
+              padding: "20px",
+            }}
+          >
+            <label>
+              Time Duration (in months):
+              <Select
+                onChange={handleOption1Change}
+                placeholder="Expected project duration (in months)"
+                required
+              >
+                <option value="short(1-5)">short(1-5)</option>
+                <option value="Medium (5-8)"> Medium (5-8)</option>
+                <option value="long(more than 8 months)">
+                  long(more than 8 months)
+                </option>
+              </Select>
+            </label>
+            <br />
+            <label>
+              Team Size:
+              <Select
+                onChange={handleOption2Change}
+                placeholder="Select your team size"
+                required
+              >
+                <option value="Very small (1-5)"> Very small (1-5)</option>
+                <option value="Small (5-10)"> Small (5-10)</option>
+                <option value="Medium (10-50)"> Medium (10-50)</option>
+                <option value="Large (More than 50)">
+                  {" "}
+                  Large (More than 50)
+                </option>
+              </Select>
+            </label>
+            <br />
+            <div>
+              <label>
+                App Idea:
+                <Input
+                  w="xl"
+                  type="text"
+                  list="colleges"
+                  placeholder="Select your project idea"
+                  onChange={(e) => setInputValue(e.target.value)}
+                  required
+                />
+                <datalist id="colleges">
+                  {filteredIdea.map((college) => (
+                    <option value={college} key={college} />
+                  ))}
+                </datalist>
+              </label>
+            </div>
 
-      <div>
-        <label>Project Type:</label>
-        <label>
-          <input
-            type="checkbox"
-            checked={isWeb}
-            value="Web"
-            onChange={handleWebCheckboxChange}
-          />
-          Web
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            checked={isApp}
-            value="App"
-            onChange={handleAppCheckboxChange}
-          />
-          App
-        </label>
-      </div>
+            <Text fontSize="2xl">Designing skills</Text>
+            <div>
+              {questions.map((q) => (
+                <div key={q.id}>
+                  <p>{q.question}</p>
+                  {q.options.map((option) => (
+                    <div key={option.value}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          id={q.id}
+                          value={option.value}
+                          checked={answers[q.id] === option.value}
+                          onChange={() =>
+                            handleAnswerChange(q.id, option.value)
+                          }
+                        />
+                        {option.text}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
 
-      <Button colorScheme='teal' onClick={() => add()} type="submit">Next</Button>
-    </form>
-    </Center>
-    </Box>
+            <div>
+              <label>Project Type:</label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isWeb}
+                  value="Web"
+                  onChange={handleWebCheckboxChange}
+                />
+                Web
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isApp}
+                  value="App"
+                  onChange={handleAppCheckboxChange}
+                />
+                App
+              </label>
+            </div>
+
+            <Button colorScheme="teal" onClick={() => add()} type="submit">
+              Next
+            </Button>
+          </form>
+        </Center>
+      </Box>
     </>
   );
 }
